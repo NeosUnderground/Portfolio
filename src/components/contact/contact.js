c8992890aa6f4fbc5994b7e657750506a9de6495
NeosUnderground <33915360+NeosUnderground@users.noreply.github.com>
2019-05-23 11:28:02 -0500

    'commit'

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,6 +8,7 @@ const Contact=()=>{
         <a a href="mailto:james.f.kubitz@gmail.com"><div className='br4 shadow-2 pa2 ma1 grow bounce-6 badge1'><img src={require('./contactImages/Gmail.jpg')} style={{width:"40px", height:"40px"}} alt='GmailImage'/></div></a>
         <a href='https://github.com/NeosUnderground'  target='blank'><div className='br4 shadow-2 pa2 ma3 grow bounce-6 badge1'><img src={require('./contactImages/Github.png')} style={{width:"40px", height:"40px"}} alt='GithubImage'/></div></a>
         <a href='https://www.linkedin.com/in/jameskubitz/' target='blank'><div className='br4 shadow-2 pa2 ma1 grow bounce-6 badge1'><img src={require('./contactImages/Linkedin.jpg')} style={{width:"40px", height:"40px"}} alt='LinkedinImage'/></div></a>
+        <a href="https://dev.to/neosunderground"><div className='br4 shadow-2 pa2 ma1 grow bounce-6 badge1'><img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" style={{width:"40px", height:"40px"}} alt="NeosUnderground's DEV Profile"/></div></a>
       </div>
       <h4 className='grow'><a  className='linkToTop' href='#about'>To the top!</a></h4>
       <p className='white'> Copyright &#169; 2019 - James Kubitz </p>
